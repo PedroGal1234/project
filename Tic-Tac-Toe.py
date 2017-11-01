219b8ca1c0db4a72ab8d9b4fc53570337a7cb523
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-01 13:59:52 -0400

    Updated from Brython Server: 11/1/2017 1:59:51 PM

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -7,11 +7,18 @@ from ggame import *
 black = Color(0x00000,1)
 blackOutline = LineStyle(10,black)
 
-
-
+def click(event)
 
 
 if __name__ == '__main__':
-    board1 = LineAsset(0,300,blackOutline)
-    board2
-
+    black = Color(0x00000,1)
+    blackOutline = LineStyle(10,black)
+    boardV1 = LineAsset(0,450,blackOutline)
+    boardH1 = LineAsset(450,0,blackOutline)
+    
+    Sprite(boardV1,(200,50))
+    Sprite(boardV1,(350,50))
+    Sprite(boardH1,(50,200))
+    Sprite(boardH1,(50,350))
+    
+    App().run()
