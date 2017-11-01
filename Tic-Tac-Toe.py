b53c5ccbf26617d1a082676fc875f85de5de875d
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-01 13:48:37 -0400

    Updated from Brython Server: 11/1/2017 1:48:36 PM

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,1 +1,17 @@
+#Pedro Gallino
+#11/1/17
+#tic-tac-toe against computer
+
+from ggame import *
+
+black = Color(0x00000,1)
+blackOutline = LineStyle(10,black)
+
+
+
+
+
+if __name__ == '__main__':
+    board1 = LineAsset(0,300,blackOutline)
+    board2
 
