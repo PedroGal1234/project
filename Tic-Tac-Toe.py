f80bd2c1dcd0bd76caa2492163d880f800917cd4
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-02 11:08:00 -0400

    Updated from Brython Server: 11/2/2017 11:07:59 AM

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -7,7 +7,12 @@ from ggame import *
 black = Color(0x00000,1)
 blackOutline = LineStyle(10,black)
 
-def click(event)
+#Does SOmething when someone clicks the hand
+def mouseClick(event):
+    if event.x < 200 and event.x > 50:
+        if event.y < 200 and event.y > 50:
+            Sprite(x1,(50,50))
+            Sprite(x2,(50,100))
 
 
 if __name__ == '__main__':
@@ -16,9 +21,14 @@ if __name__ == '__main__':
     boardV1 = LineAsset(0,450,blackOutline)
     boardH1 = LineAsset(450,0,blackOutline)
     
+    x1 = LineAsset(40,-40,blackOutline)
+    x2 = LineAsset(40,40,blackOutline)
+    
+    #Desplays the board
     Sprite(boardV1,(200,50))
     Sprite(boardV1,(350,50))
     Sprite(boardH1,(50,200))
     Sprite(boardH1,(50,350))
     
+    App().listenMouseEvent('click',mouseClick)
     App().run()
