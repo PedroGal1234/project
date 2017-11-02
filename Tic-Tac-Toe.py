21099595685d14af645a554ab3db2d143ae5519d
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-02 14:53:30 -0400

    Updated from Brython Server: 11/2/2017 2:53:29 PM

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -3,51 +3,63 @@
 #tic-tac-toe against computer
 
 from ggame import *
-from math import randint
+from random import randint
 
 #Does Something when someone clicks On an open Sace
 def mouseClick(event):
 #Prints out things when clicks in first column
     if event.x < 200 and event.x > 50:
         if event.y < 200 and event.y > 50:
-            Sprite(x1,(80,175))
-            Sprite(x2,(80,75))
+            Sprite(data['x1'],(80,175))
+            Sprite(data['x2'],(80,75))
         elif event.y < 350 and event.y > 200:
-            Sprite(x1,(80,320))
-            Sprite(x2,(80,220))
+            Sprite(data['x1'],(80,320))
+            Sprite(data['x2'],(80,220))
         elif event.y < 650 and event.y > 350:
-            Sprite(x1,(80,465))
-            Sprite(x2,(80,365))
+            Sprite(data['x1'],(80,465))
+            Sprite(data['x2'],(80,365))
 #Prints out things when clicks in second column  
     elif event.x < 350 and event.x > 50:
         if event.y < 200 and event.y > 50:
-            Sprite(x1,(225,175))
-            Sprite(x2,(225,75))
+            Sprite(data['x1'],(225,175))
+            Sprite(data['x2'],(225,75))
         elif event.y < 350 and event.y > 200:
-            Sprite(x1,(225,320))
-            Sprite(x2,(225,220))
+            Sprite(data['x1'],(225,320))
+            Sprite(data['x2'],(225,220))
         elif event.y < 650 and event.y > 350:
-            Sprite(x1,(225,465))
-            Sprite(x2,(225,365))
+            Sprite(data['x1'],(225,465))
+            Sprite(data['x2'],(225,365))
 #Prints out things when clicks in third column
     elif event.x < 500 and event.x > 350:
         if event.y < 200 and event.y > 50:
-            Sprite(x1,(370,175))
-            Sprite(x2,(370,75))
+            Sprite(data['x1'],(370,175))
+            Sprite(data['x2'],(370,75))
         elif event.y < 350 and event.y > 200:
-            Sprite(x1,(370,320))
-            Sprite(x2,(370,220))
+            Sprite(data['x1'],(370,320))
+            Sprite(data['x2'],(370,220))
         elif event.y < 650 and event.y > 350:
-            Sprite(x1,(370,465))
-            Sprite(x2,(370,365))
+            Sprite(data['x1'],(370,465))
+            Sprite(data['x2'],(370,365))
     
     place = randint(1,9)
-        if place == 1:
-            sprite(o,(100,100)
-        if place == 2:
-        
-            if place == 2:
-    
+    if place == 1:
+        Sprite(data['o'],(110,110))
+    elif place == 2:
+        Sprite(data['o'],(275,110))
+    elif place == 3:
+        Sprite(data['o'],(425,110))
+    elif place == 4:
+        Sprite(data['o'],(110,275))
+    elif place == 5:
+        Sprite(data['o'],(275,275))
+    elif place == 6:
+        Sprite(data['o'],(425,275))
+    elif place == 7:
+        Sprite(data['o'],(110,450))
+    elif place == 8:
+        Sprite(data['o'],(275,425))
+    elif place == 9:
+        Sprite(data['o'],(425,425))
     
 
 
@@ -57,11 +69,20 @@ if __name__ == '__main__':
     blackOutline = LineStyle(10,black)
     boardV1 = LineAsset(0,450,blackOutline)
     boardH1 = LineAsset(450,0,blackOutline)
+    data = {}
     
-    x1 = LineAsset(100,-100,LineStyle(8,black))
-    x2 = LineAsset(100,100,LineStyle(8,black))
-    o = CircleAsset(50,LineStyle(8,black),white)
-    
+    data['x1'] = LineAsset(100,-100,LineStyle(8,black))
+    data['x2'] = LineAsset(100,100,LineStyle(8,black))
+    data['o'] = CircleAsset(50,LineStyle(8,black),white)
+    data['s1'] = 0
+    data['s2'] = 0
+    data['s3'] = 0
+    data['s4'] = 0
+    data['s5'] = 0
+    data['s6'] = 0
+    data['s7'] = 0
+    data['s8'] = 0
+    data['s9'] = 0
     #Desplays the board
     Sprite(boardV1,(200,50))
     Sprite(boardV1,(350,50))
