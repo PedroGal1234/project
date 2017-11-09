83c22bd109fd3ec3a4ba56daa0d66a7ab1713ba5
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-09 11:07:29 -0500

    Updated from Brython Server: 11/9/2017 11:07:28 AM

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -14,56 +14,108 @@ def isEmpty(s):
 
 #Checks to see if someone won
 def winner():
+    red = Color(0xff0000,.25)
+    redOutline = LineStyle(1,red)
+    redRectangle = RectangleAsset(150,150,redOutline,red)
+    
     #Checks to see if X won
     if data['s1'] == 1 and data['s2'] == 1 and data['s3'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,50))
+        Sprite(redRectangle,(200,50))
+        Sprite(redRectangle,(350,50))
     elif data['s4'] == 1 and data['s5'] == 1 and data['s6'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,200))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(350,200))
     elif data['s7'] == 1 and data['s8'] == 1 and data['s9'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,350))
+        Sprite(redRectangle,(200,350))
+        Sprite(redRectangle,(350,350))
     elif data['s1'] == 1 and data['s4'] == 1 and data['s7'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,50))
+        Sprite(redRectangle,(50,200))
+        Sprite(redRectangle,(50,350))
     elif data['s2'] == 1 and data['s5'] == 1 and data['s8'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(200,50))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(200,350))
     elif data['s3'] == 1 and data['s6'] == 1 and data['s9'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(350,50))
+        Sprite(redRectangle,(350,200))
+        Sprite(redRectangle,(350,350))
     elif data['s1'] == 1 and data['s5'] == 1 and data['s9'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,50))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(350,350))
     elif data['s7'] == 1 and data['s5'] == 1 and data['s3'] == 1:
         data['say'] = 'X wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(350,50))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(350,350))
     #Checks to see if O won
-    elif data['s1'] == 2 and data['s2'] == 2 and data['s3'] == 2:
+    if data['s1'] == 2 and data['s2'] == 2 and data['s3'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,50))
+        Sprite(redRectangle,(200,50))
+        Sprite(redRectangle,(350,50))
     elif data['s4'] == 2 and data['s5'] == 2 and data['s6'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,200))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(350,200))
     elif data['s7'] == 2 and data['s8'] == 2 and data['s9'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,350))
+        Sprite(redRectangle,(200,350))
+        Sprite(redRectangle,(350,350))
     elif data['s1'] == 2 and data['s4'] == 2 and data['s7'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,50))
+        Sprite(redRectangle,(50,200))
+        Sprite(redRectangle,(50,350))
     elif data['s2'] == 2 and data['s5'] == 2 and data['s8'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(200,50))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(200,350))
     elif data['s3'] == 2 and data['s6'] == 2 and data['s9'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(350,50))
+        Sprite(redRectangle,(350,200))
+        Sprite(redRectangle,(350,350))
     elif data['s1'] == 2 and data['s5'] == 2 and data['s9'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(50,50))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(350,350))
     elif data['s7'] == 2 and data['s5'] == 2 and data['s3'] == 2:
         data['say'] = 'O wins'
         data['gameOver'] = True
+        Sprite(redRectangle,(350,50))
+        Sprite(redRectangle,(200,200))
+        Sprite(redRectangle,(50,350))
 
 #Checks to see if a tie
     elif fullboard() == True:
@@ -83,51 +135,51 @@ def mouseClick(event):
     if data['gameOver'] == False:
         #Prints out things when clicks in first column
         if event.x < 200 and event.x > 50:
-            if event.y < 200 and event.y > 50:
+            if event.y < 200 and event.y > 50 and isEmpty(1) == True:
                 Sprite(data['x1'],(80,175))
                 Sprite(data['x2'],(80,75))
                 data['s1'] = 1
                 ComputerTurn()
-            elif event.y < 350 and event.y > 200:
+            elif event.y < 350 and event.y > 200 and isEmpty(4) == True:
                 Sprite(data['x1'],(80,320))
                 Sprite(data['x2'],(80,220))
                 data['s4'] = 1
                 ComputerTurn()
-            elif event.y < 650 and event.y > 350:
+            elif event.y < 650 and event.y > 350 and isEmpty(7) == True:
                 Sprite(data['x1'],(80,465))
                 Sprite(data['x2'],(80,365))
                 data['s7'] = 1
                 ComputerTurn()
     #Prints out things when clicks in second column  
         elif event.x < 350 and event.x > 50:
-            if event.y < 200 and event.y > 50:
+            if event.y < 200 and event.y > 50 and isEmpty(2) == True:
                 Sprite(data['x1'],(225,175))
                 Sprite(data['x2'],(225,75))
                 data['s2'] = 1
                 ComputerTurn()
-            elif event.y < 350 and event.y > 200:
+            elif event.y < 350 and event.y > 200 and isEmpty(5) == True:
                 Sprite(data['x1'],(225,320))
                 Sprite(data['x2'],(225,220))
                 data['s5'] = 1
                 ComputerTurn()
-            elif event.y < 650 and event.y > 350:
+            elif event.y < 650 and event.y > 350 and isEmpty(8) == True:
                 Sprite(data['x1'],(225,465))
                 Sprite(data['x2'],(225,365))
                 data['s8'] = 1
                 ComputerTurn()
     #Prints out things when clicks in third column
         elif event.x < 500 and event.x > 350:
-            if event.y < 200 and event.y > 50:
+            if event.y < 200 and event.y > 50 and isEmpty(3) == True:
                 Sprite(data['x1'],(370,175))
                 Sprite(data['x2'],(370,75))
                 data['s3'] = 1
                 ComputerTurn()
-            elif event.y < 350 and event.y > 200:
+            elif event.y < 350 and event.y > 200 and isEmpty(6) == True:
                 Sprite(data['x1'],(370,320))
                 Sprite(data['x2'],(370,220))
                 data['s6'] = 1
                 ComputerTurn()
-            elif event.y < 650 and event.y > 350:
+            elif event.y < 650 and event.y > 350 and isEmpty(9) == True:
                 Sprite(data['x1'],(370,465))
                 Sprite(data['x2'],(370,365))
                 data['s9'] = 1
